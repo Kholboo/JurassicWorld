6ab7f36ea76c6df647cdf7327de03b32e2c96915
Tugsbileg N <ntugsbileg@alchemist.mn>
2019-11-08 14:48:57 +0800

    refactored

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -7,13 +7,15 @@ public class PanelManager : MonoBehaviour {
     public List<GameObject> panels = new List<GameObject> ();
 
     public void ChangePanelState (Panels panelName, bool state = false, float delay = 0.0f) {
-        GameObject panel = panels.Find (p => p.name == panelName.ToString ());
-
-        StartCoroutine (_ChangePanelState (panel, state, delay));
+        StartCoroutine (_ChangePanelState (FindPanel (panelName), state, delay));
     }
 
     IEnumerator _ChangePanelState (GameObject panel, bool state, float time) {
         yield return new WaitForSeconds (time);
         panel.SetActive (state);
+    }
+
+    public GameObject FindPanel (Panels panel) {
+        return panels.Find (p => p.name == panel.ToString ());
     }
 }
