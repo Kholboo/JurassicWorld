1a2ad1a5158c5b61c3b3c67d6948c21858010047
Tugsbileg N <ntugsbileg@alchemist.mn>
2019-11-08 16:20:45 +0800

    fixed bug

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -3,18 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour {
-    PanelManager panelManager;
-
-    void Start () {
-        GameObject canvas = GameObject.FindGameObjectWithTag ("Canvas");
-        panelManager = canvas.GetComponent<PanelManager> ();
-    }
-
     void OnTriggerEnter (Collider col) {
         if (col.gameObject.tag == "Player") {
-            panelManager.ChangePanelState (PanelManager.Panels.HUDPanel, false);
+            GameManager.Instance.panelManager.ChangePanelState (PanelManager.Panels.HUDPanel, false);
             GameManager.Instance.SetState (GameManager.States.Completed);
-            panelManager.ChangePanelState (PanelManager.Panels.LevelCompletePanel, true);
+            GameManager.Instance.panelManager.ChangePanelState (PanelManager.Panels.LevelCompletePanel, true);
         }
     }
 }
