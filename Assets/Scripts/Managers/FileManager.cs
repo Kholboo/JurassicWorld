a702de091dbd6d17e85756ff2baedec0a6b64f19
Tugsbileg N <tugsbileg@Tugsbilegs-MacBook-Pro.local>
2020-07-22 15:44:09 +0800

    refactoring

--- Assets/Scripts/Managers/FileManager.cs
+++ Assets/Scripts/Managers/FileManager.cs
@@ -1,18 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class FileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    string dataPath = "/file.data";
+
     void Start()
     {
-        
+        if (!File.Exists(Application.persistentDataPath + dataPath))
+        {
+            Save();
+            return;
+        }
+
+        Load();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Save()
     {
-        
+        FileStream file = null;
+
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            file = File.Create(Application.persistentDataPath + dataPath);
+
+            // bf.Serialize(file, data...);
+        }
+        catch (System.Exception e)
+        {
+            if (e != null)
+            {
+                Debug.LogError(e);
+            }
+        }
+        finally
+        {
+            if (file != null)
+            {
+                file.Close();
+            }
+        }
+    }
+
+    public void Load()
+    {
+        FileStream file = null;
+
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            file = File.Open(Application.persistentDataPath + dataPath, FileMode.Open);
+
+            // data... = (DataType) bf.Deserialize(file);
+        }
+        catch (System.Exception e)
+        {
+            if (e != null)
+            {
+                Debug.LogError(e);
+            }
+        }
+        finally
+        {
+            if (file != null)
+            {
+                file.Close();
+            }
+        }
+    }
+
+    public void Delete()
+    {
+        File.Delete(Application.persistentDataPath + dataPath);
     }
 }
