a102c4b99b9938b129ed1da212b27b39172c226f
Tugsbileg N <tugsbileg@Tugsbilegs-MacBook-Pro.local>
2020-07-24 12:18:17 +0800

    integrating ui

--- Assets/Scripts/Managers/Panels/HomePanel.cs
+++ Assets/Scripts/Managers/Panels/HomePanel.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HomePanel : MonoBehaviour {
-
+public class HomePanel : MonoBehaviour
+{
+    public void Play()
+    {
+        GameManager.Instance.panelManager.ChangeState(Panels.HomePanel, false);
+        GameManager.Instance.SetState(GameState.Play);
+        GameManager.Instance.panelManager.ChangeState(Panels.HUDPanel, true);
+    }
 }
