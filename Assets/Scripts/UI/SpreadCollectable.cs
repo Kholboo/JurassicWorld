a4b334f774d762ed55f935c253060dffa0536946
Tugsbileg N <tugsbileg@Tugsbilegs-MacBook-Pro.local>
2020-07-30 17:42:56 +0800

    spread collectable instan

--- Assets/Scripts/UI/SpreadCollectable.cs
+++ Assets/Scripts/UI/SpreadCollectable.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpreadCollectable : MonoBehaviour {
-    public static SpreadCollectable instance;
+public class SpreadCollectable : MonoBehaviour
+{
     public GameObject spawnObject, targetObject;
-    [Range (0, 500)]
+    [Range(0, 500)]
     public int spreadRangeX, spreadRangeY;
-    [Range (1, 15)]
+    [Range(1, 15)]
     public int speed, spreadSpeed;
-    [Range (0, 2)]
+    [Range(0, 2)]
     public float startSize;
     public int spawnCount;
     [HideInInspector]
     public float delay;
 
-    void Awake () {
-        instance = this;
-        speed = Random.Range (8, 10);
-        spreadSpeed = Random.Range (2, 4);
+    void Awake()
+    {
+        speed = Random.Range(8, 10);
+        spreadSpeed = Random.Range(2, 4);
     }
-    public void Spawn () {
-        for (int i = 0; i <= spawnCount; i++) {
-            Instantiate (spawnObject, transform.position, Quaternion.identity);
+    public void Spawn()
+    {
+        for (int i = 0; i <= spawnCount; i++)
+        {
+            GameObject _spawnObject = Instantiate(spawnObject, transform.position, Quaternion.identity);
+            _spawnObject.transform.SetParent(transform);
         }
     }
 }
--- Assets/Scripts/UI/UICoin.cs
+++ Assets/Scripts/UI/UICoin.cs
@@ -1,50 +1,60 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class UICoin : MonoBehaviour {
+public class UICoin : MonoBehaviour
+{
+    SpreadCollectable spreadCollectable;
     bool isSpread = true;
     Vector3 euler, spreadPositon;
     float randomDelay, time;
     int repeatTime;
     Animator animator;
 
-    void Start () {
+    void Start()
+    {
+        spreadCollectable = GetComponentInParent<SpreadCollectable>();
         repeatTime = 0;
         time = 0;
         euler = transform.eulerAngles;
-        euler.z = Random.Range (0f, 360f);
-        randomDelay = Random.Range (0.8f, 1f);
-        SpreadCollectable.instance.delay = randomDelay * 2;
-        animator = SpreadCollectable.instance.targetObject.GetComponent<Animator> ();
+        euler.z = Random.Range(0f, 360f);
+        randomDelay = Random.Range(0.8f, 1f);
+        spreadCollectable.delay = randomDelay * 2;
+        animator = spreadCollectable.targetObject.GetComponent<Animator>();
         transform.eulerAngles = euler;
-        transform.localScale = new Vector3 (SpreadCollectable.instance.startSize, SpreadCollectable.instance.startSize, SpreadCollectable.instance.startSize);
-        StartCoroutine ("WaitForSend");
+        transform.localScale = new Vector3(spreadCollectable.startSize, spreadCollectable.startSize, spreadCollectable.startSize);
+        StartCoroutine("WaitForSend");
     }
 
-    void LateUpdate () {
-        if (isSpread) {
-            Spread ();
+    void LateUpdate()
+    {
+        if (isSpread)
+        {
+            Spread();
         }
-        if (!isSpread) {
-            transform.localPosition = Vector3.Lerp (transform.localPosition, Vector3.zero, SpreadCollectable.instance.speed * Time.deltaTime);
-            transform.localRotation = Quaternion.Lerp (transform.localRotation, Quaternion.identity, SpreadCollectable.instance.speed * Time.deltaTime);
-            if (transform.localScale.x >= 0) {
-                transform.localScale -= new Vector3 (Time.deltaTime, Time.deltaTime, Time.deltaTime);
+        if (!isSpread)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.zero, spreadCollectable.speed * Time.deltaTime);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.identity, spreadCollectable.speed * Time.deltaTime);
+            if (transform.localScale.x >= 0)
+            {
+                transform.localScale -= new Vector3(Time.deltaTime, Time.deltaTime, Time.deltaTime);
             }
         }
     }
-    public void Spread () {
-        transform.localPosition = Vector3.Lerp (transform.localPosition, spreadPositon, SpreadCollectable.instance.spreadSpeed * Time.deltaTime);
+    public void Spread()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, spreadPositon, spreadCollectable.spreadSpeed * Time.deltaTime);
     }
-    IEnumerator WaitForSend () {
-        Destroy (gameObject, 2.5f);
-        transform.SetParent (SpreadCollectable.instance.transform);
-        spreadPositon = new Vector3 (transform.localPosition.x + Random.Range (-SpreadCollectable.instance.spreadRangeX, SpreadCollectable.instance.spreadRangeX), transform.localPosition.y + Random.Range (-SpreadCollectable.instance.spreadRangeY, SpreadCollectable.instance.spreadRangeY), transform.localPosition.z);
-        yield return new WaitForSeconds (randomDelay);
-        transform.SetParent (SpreadCollectable.instance.targetObject.transform);
+    IEnumerator WaitForSend()
+    {
+        Destroy(gameObject, 2.5f);
+        transform.SetParent(spreadCollectable.transform);
+        spreadPositon = new Vector3(transform.localPosition.x + Random.Range(-spreadCollectable.spreadRangeX, spreadCollectable.spreadRangeX), transform.localPosition.y + Random.Range(-spreadCollectable.spreadRangeY, spreadCollectable.spreadRangeY), transform.localPosition.z);
+        yield return new WaitForSeconds(randomDelay);
+        transform.SetParent(spreadCollectable.targetObject.transform);
         isSpread = false;
-        yield return new WaitForSeconds (randomDelay);
-        animator.Play ("CollectableScale");
-        GameManager.Instance.tapticManager.Impact (HapticTypes.HeavyImpact);
+        yield return new WaitForSeconds(randomDelay);
+        animator.Play("CollectableScale");
+        GameManager.Instance.tapticManager.Impact(HapticTypes.HeavyImpact);
     }
 }
