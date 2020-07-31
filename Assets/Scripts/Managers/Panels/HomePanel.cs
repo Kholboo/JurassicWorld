bd0e8ccdd21542874983a414b4f1441cdb43f2c3
Myagmarsuren Sh <myagmarsuren.sh@alchemist.mn>
2020-07-31 18:03:24 +0800

    a

--- Assets/Scripts/Managers/Panels/HomePanel.cs
+++ Assets/Scripts/Managers/Panels/HomePanel.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class HomePanel : MonoBehaviour {
     public GameObject title, button;
     private void OnEnable () {
-        StartCoroutine (WaitAndEnable (1f, title));
-        StartCoroutine (WaitAndEnable (2f, button));
+        StartCoroutine (WaitAndEnable (0.4f, title));
+        StartCoroutine (WaitAndEnable (0.6f, button));
     }
     public void Play () {
-        GetComponent<Animator> ().Play ("ButtonPress");
+        GetComponent<Animator> ().Play ("FadeOut");
         GameManager.Instance.panelManager.ChangeState (Panels.HomePanel, false, 1f);
         GameManager.Instance.SetState (GameState.Play);
         GameManager.Instance.panelManager.ChangeState (Panels.HUDPanel, true, 1f);
