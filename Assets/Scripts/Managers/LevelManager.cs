4f22bc2d048dcb03dd2bd1f235e23a83b10d8fb6
Tugsbileg N <Tugsbileg>
2020-11-10 13:41:27 +0800

    added loading scene

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -3,70 +3,89 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour {
-    public List<GameObject> levels = new List<GameObject> ();
-    public List<GameObject> bonusLevels = new List<GameObject> ();
+public class LevelManager : MonoBehaviour
+{
+    public List<GameObject> levels = new List<GameObject>();
+    public List<GameObject> bonusLevels = new List<GameObject>();
     public int randomIndex = 0;
     bool isBonusLevel;
-    public bool IsBonusLevel {
+    public bool IsBonusLevel
+    {
         get { return isBonusLevel; }
     }
     public bool hasBonusLevel;
-    [ShowIf ("hasBonusLevel", true)]
+    [ShowIf("hasBonusLevel", true)]
     public int bonusLevelStep = 3;
 
-    public void SpawnLevel () {
-        if (CheckBonusLevel ()) {
-            Instantiate (bonusLevels[FindLevelIndex ()], Vector3.zero, Quaternion.identity);
-        } else {
-            Instantiate (levels[FindLevelIndex ()], Vector3.zero, Quaternion.identity);
+    public void SpawnLevel()
+    {
+        if (CheckBonusLevel())
+        {
+            Instantiate(bonusLevels[FindLevelIndex()], Vector3.zero, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(levels[FindLevelIndex()], Vector3.zero, Quaternion.identity);
         }
     }
 
-    bool CheckBonusLevel () {
-        return isBonusLevel = hasBonusLevel && GetSession () % bonusLevelStep == 0;
+    bool CheckBonusLevel()
+    {
+        return isBonusLevel = hasBonusLevel && GetSession() % bonusLevelStep == 0;
     }
 
-    int FindLevelIndex () {
-        if (isBonusLevel) {
-            return Random.Range (0, bonusLevels.Count);
-        } else {
-            return GetLevelIndex ();
+    int FindLevelIndex()
+    {
+        if (isBonusLevel)
+        {
+            return Random.Range(0, bonusLevels.Count);
+        }
+        else
+        {
+            return GetLevelIndex();
         }
     }
 
-    int GetLevelIndex () {
-        return PlayerPrefs.GetInt ("LevelIndex", 0);
+    int GetLevelIndex()
+    {
+        return PlayerPrefs.GetInt("LevelIndex", 0);
     }
 
-    int GenerateRandomIndex () {
-        int index = Random.Range (randomIndex, levels.Count);
-        return index == GetLevelIndex () ? GenerateRandomIndex () : index;
+    int GenerateRandomIndex()
+    {
+        int index = Random.Range(randomIndex, levels.Count);
+        return index == GetLevelIndex() ? GenerateRandomIndex() : index;
     }
 
-    public void SaveLevel () {
-        if (!isBonusLevel) {
-            PlayerPrefs.SetInt ("Level", GetLevel () + 1);
-            PlayerPrefs.SetInt ("LevelIndex", GetLevel () > levels.Count ? GenerateRandomIndex () : GetLevelIndex () + 1);
+    public void SaveLevel()
+    {
+        if (!isBonusLevel)
+        {
+            PlayerPrefs.SetInt("Level", GetLevel() + 1);
+            PlayerPrefs.SetInt("LevelIndex", GetLevel() > levels.Count ? GenerateRandomIndex() : GetLevelIndex() + 1);
         }
 
-        PlayerPrefs.SetInt ("Session", GetSession () + 1);
-        LevelUp (true);
+        PlayerPrefs.SetInt("Session", GetSession() + 1);
+        LevelUp(true);
     }
 
-    int GetSession () {
-        return PlayerPrefs.GetInt ("Session", 1);
+    int GetSession()
+    {
+        return PlayerPrefs.GetInt("Session", 1);
     }
 
-    public int GetLevel () {
-        return PlayerPrefs.GetInt ("Level", 1);
+    public int GetLevel()
+    {
+        return PlayerPrefs.GetInt("Level", 1);
     }
 
-    public void LevelUp (bool state = false) {
-        PlayerPrefs.SetInt ("LevelUp", state ? 1 : 0);
+    public void LevelUp(bool state = false)
+    {
+        PlayerPrefs.SetInt("LevelUp", state ? 1 : 0);
     }
 
-    public bool IsLevelUp () {
-        return PlayerPrefs.GetInt ("LevelUp", 0) == 1;
+    public bool IsLevelUp()
+    {
+        return PlayerPrefs.GetInt("LevelUp", 0) == 1;
     }
 }
---
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public CanvasGroup canvasGroup;
+    public Text loadingProgressText;
+    public Image loadingSlider;
+
+    public void Awake()
+    {
+        StartCoroutine(StartLoad());
+    }
+
+    IEnumerator StartLoad()
+    {
+        loadingScreen.SetActive(true);
+        AsyncOperation async = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        async.allowSceneActivation = false;
+
+        while (!async.isDone)
+        {
+            float progress = async.progress / 0.9f;
+            loadingSlider.fillAmount = progress;
+            loadingProgressText.text = (progress * 100.0f + "%").ToString();
+
+            if (async.progress >= 0.9f)
+            {
+                loadingSlider.fillAmount = 1f;
+                loadingProgressText.text = "100%";
+                async.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+
+        yield return StartCoroutine(FadeLoadingScreen(0, 1));
+        loadingScreen.SetActive(false);
+    }
+
+    IEnumerator FadeLoadingScreen(float targetValue, float duration)
+    {
+        float startValue = canvasGroup.alpha;
+        float time = 0;
+
+        while (time < duration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(startValue, targetValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        canvasGroup.alpha = targetValue;
+    }
+}
