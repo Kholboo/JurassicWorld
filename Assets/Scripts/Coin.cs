230d1675bde9847a7944d414bc0966ec0f882193
Tugsbileg <ntugsbileg@alchemist.mn>
2021-02-26 12:15:14 +0800

    fixed coin

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 
-public class Coin : MonoBehaviour {
+public class Coin : MonoBehaviour
+{
     public GameObject increaseText, particle;
     public bool showText, showParticle;
-    private void OnTriggerEnter (Collider other) {
-        if (other.gameObject.tag == "Player") {
-            Destroy (gameObject, 0.45f);
-            GameManager.Instance.coinManager.UpdateCoin (1);
-            GameManager.Instance.tapticManager.Impact (HapticTypes.MediumImpact);
-            GetComponent<Animator> ().enabled = true;
-            GetComponent<Animator> ().Play ("CoinPickUp");
-            GetComponent<BoxCollider> ().enabled = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject, 0.45f);
+            GameManager.Instance.coinManager.UpdateCoin(1);
+            GameManager.Instance.tapticManager.Impact(HapticTypes.MediumImpact);
+            GetComponentInChildren<Animator>().enabled = true;
+            GetComponentInChildren<Animator>().Play("CoinPickUp");
+            GetComponent<BoxCollider>().enabled = false;
             if (showText)
-                Instantiate (increaseText, transform.position, Quaternion.identity);
-            if (showParticle) {
-                Instantiate (particle, transform.position, Quaternion.identity);
+                Instantiate(increaseText, transform.position, Quaternion.identity);
+            if (showParticle)
+            {
+                Instantiate(particle, transform.position, Quaternion.identity);
             }
         }
     }
