81fa11bcf769142bb7ff020400fb555b81950265
Tugsbileg <ntugsbileg@alchemist.mn>
2022-02-10 17:17:59 +0800

    changed nicevibration to 4.1.0

--- Assets/Scripts/Managers/TapticManager.cs
+++ Assets/Scripts/Managers/TapticManager.cs
@@ -1,33 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Lofelt.NiceVibrations;
 using UnityEngine;
-using MoreMountains.NiceVibrations;
-public class TapticManager : MonoBehaviour
-{
-    public void ImpactPattern(HapticTypes type)
-    {
-        switch (type)
-        {
+
+public enum HapticTypes {
+    LightImpact,
+    MediumImpact,
+    HeavyImpact,
+    Failure,
+    Success,
+}
+
+public class TapticManager : MonoBehaviour {
+    public void ImpactPattern (HapticTypes type) {
+        switch (type) {
             case HapticTypes.Failure:
-                Impact(HapticTypes.Failure);
+                Impact (HapticTypes.Failure);
                 break;
             case HapticTypes.Success:
-                Impact(HapticTypes.LightImpact, 0.1f, 5);
+                Impact (HapticTypes.LightImpact, 0.1f, 5);
                 break;
         }
     }
 
-    public void Impact(HapticTypes type = HapticTypes.MediumImpact, float delay = 0.0f, int count = 1)
-    {
-        StartCoroutine(_Impact(type, delay, count));
+    public void Impact (HapticTypes type = HapticTypes.MediumImpact, float delay = 0.0f, int count = 1) {
+        if (DeviceCapabilities.isVersionSupported) {
+            StartCoroutine (_Impact (type, delay, count));
+        }
     }
 
-    IEnumerator _Impact(HapticTypes type, float delay, int count)
-    {
-        for (int i = 0; i < count; i++)
-        {
-            MMVibrationManager.Haptic(type);
-            yield return new WaitForSeconds(delay);
+    IEnumerator _Impact (HapticTypes type, float delay, int count) {
+        for (int i = 0; i < count; i++) {
+            HapticPatterns.PlayPreset (GetType (type));
+            yield return new WaitForSeconds (delay);
         }
+    }
+
+    public HapticPatterns.PresetType GetType (HapticTypes type) {
+        HapticPatterns.PresetType hapticPattern = HapticPatterns.PresetType.LightImpact;;
+
+        switch (type) {
+            case HapticTypes.LightImpact:
+                hapticPattern = HapticPatterns.PresetType.LightImpact;
+                break;
+            case HapticTypes.MediumImpact:
+                hapticPattern = HapticPatterns.PresetType.MediumImpact;
+                break;
+            case HapticTypes.HeavyImpact:
+                hapticPattern = HapticPatterns.PresetType.HeavyImpact;
+                break;
+            case HapticTypes.Failure:
+                hapticPattern = HapticPatterns.PresetType.Failure;
+                break;
+            case HapticTypes.Success:
+                hapticPattern = HapticPatterns.PresetType.Success;
+                break;
+        }
+
+        return hapticPattern;
     }
 }
